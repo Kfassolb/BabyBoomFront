69c9ac5e304ec256f2216e743ead578b6caa3d5c
SheylaQuispe <u201620209@upc.edu.pe>
2023-04-12 19:25:50 -0500

    test

--- BabyBoom/src/app/app.module.ts
+++ BabyBoom/src/app/app.module.ts
@@ -12,7 +12,6 @@ import {MatTableModule} from '@angular/material/table';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { UsuarioListarComponent } from './components/usuario/usuario-listar/usuario-listar.component'
 
-
 @NgModule({
   declarations: [
     AppComponent,
