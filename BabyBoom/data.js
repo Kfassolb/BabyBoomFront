007da2b27c3ecf8b209491eeed82b6f5f86eff12
Limber <89276826+Kfassolb@users.noreply.github.com>
2023-04-14 19:51:19 -0500

    CreaEdita Usuario Terminado

--- BabyBoom/data.js
+++ BabyBoom/data.js
@@ -2,22 +2,22 @@ module.exports = function () {
   var data = {
     Usuario: [
       {
-        IDusuario: 1,
+        id: 1,
         Username: "Parka",
         Password: "FUw$^uDnW5&i8c",
       },
       {
-        IDusuario: 2,
+        id: 2,
         Username: "Ame",
         Password: "zYP5uP^qmW^$57",
       },
       {
-        IDusuario: 3,
+        id: 3,
         Username: "Pablito",
         Password: "PLeR2!7tB8%vzo",
       },
       {
-        IDusuario: 4,
+        id: 4,
         Username: "Jachito",
         Password: "GmB!nxcbKV6s9X",
       },
--- BabyBoom/src/app/app.module.ts
+++ BabyBoom/src/app/app.module.ts
@@ -8,19 +8,34 @@ import { HttpClientModule } from '@angular/common/http';
 import {MatTableModule} from '@angular/material/table';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 import { UsuarioListarComponent } from './components/usuario/usuario-listar/usuario-listar.component'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { UsuarioCreaeditaComponent } from './components/usuario/usuario-creaedita/usuario-creaedita.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuarioListarComponent,
     UsuarioComponent,
+    UsuarioCreaeditaComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatTableModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
