513f1ae9e9d4e16fe8a3941644840536e1b8a4d4
Fabricio Iván Amarillo Zuñe <famarilloz@gmail.com>
2023-04-29 19:12:13 -0500

    barra de busqueda

--- BabyBoom/src/app/components/tipoenfermedad/tipoenfermedad-listar/tipoenfermedad-listar.component.ts
+++ BabyBoom/src/app/components/tipoenfermedad/tipoenfermedad-listar/tipoenfermedad-listar.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Tipoenfermedad } from 'src/app/model/Tipoenfermedad';
 import { TipoEnfermedadeService } from 'src/app/service/tipoenfermedad.service'
 import { MatDialog } from '@angular/material/dialog';
 import { TipoenfermedadDialogoComponent } from './tipoenfermedad-dialogo/tipoenfermedad-dialogo.component';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-tipoenfermedad-listar',
@@ -11,23 +12,22 @@ import { TipoenfermedadDialogoComponent } from './tipoenfermedad-dialogo/tipoenf
   styleUrls: ['./tipoenfermedad-listar.component.css']
 })
 export class TipoenfermedadListarComponent implements OnInit{
-filter($event: KeyboardEvent) {
-throw new Error('Method not implemented.');
-}
   lista:Tipoenfermedad[] = [];
   dataSource:MatTableDataSource<Tipoenfermedad> = new MatTableDataSource();
-  displayedColumns:string[] = ['id', 'nombreEnfermedad','TipoEnfermedad','accion01']
+  displayedColumns:string[] = ['id', 'nombreEnfermedad','TipoEnfermedad','accion01'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator; //THIS
   private idMayor: number = 0;
 
-  constructor(private tcS:TipoEnfermedadeService,private dialog:MatDialog ){
-  }
+  constructor(private tcS:TipoEnfermedadeService,private dialog:MatDialog ){}
   ngOnInit(): void {
       this.tcS.list().subscribe((data)=>{
         this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator=this.paginator;
       });
 
       this.tcS.getList().subscribe(data => {
         this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator=this.paginator;
       });
       this.tcS.getConfirmaEliminacion().subscribe(data => {
         data == true ? this.eliminar(this.idMayor) : false;
@@ -41,9 +41,12 @@ throw new Error('Method not implemented.');
       this.tcS.eliminar(id).subscribe(() => {
         this.tcS.list().subscribe(data => {
           this.tcS.setList(data);/* se ejecuta la línea 27 */
+          this.dataSource=new MatTableDataSource(data);
+          this.dataSource.paginator=this.paginator;
         });
       });
     }
+
     filtrar(e: any) {
       this.dataSource.filter = e.target.value.trim();
     }
