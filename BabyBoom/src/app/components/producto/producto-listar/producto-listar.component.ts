1693ee9e9228b4a90632d00bf17f37c8a21ae125
Stephan-5 <angiesr5@hotmail.com>
2023-05-01 00:49:57 -0500

    avance

--- BabyBoom/src/app/components/producto/producto-listar/producto-listar.component.ts
+++ BabyBoom/src/app/components/producto/producto-listar/producto-listar.component.ts
@@ -1,11 +1,11 @@
 import { Dialog } from '@angular/cdk/dialog';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild} from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Producto } from 'src/app/model/Producto';
 import { ProductoService } from 'src/app/service/producto.service';
 import { ProductoDialogoComponent } from './producto-dialogo/producto-dialogo.component';
 import { MatDialog } from '@angular/material/dialog';
-
+import { MatPaginator } from '@angular/material/paginator';
 @Component({
   selector: 'app-producto-listar',
   templateUrl: './producto-listar.component.html',
@@ -16,12 +16,15 @@ export class ProductoListarComponent implements OnInit {
   dataSource: MatTableDataSource<Producto> = new MatTableDataSource();
   displayedColumns: string[] = ['id', 'nombre', 'tipo', 'cantidad', 'preciounitario','accion1'];
   idMayor: number = 0;
+  @ViewChild(MatPaginator) paginator!: MatPaginator; //THIS
+  private idHigh:number=0;
 
   constructor(private pS: ProductoService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.pS.list().subscribe(data => {
       this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
     });
     this.pS.getList().subscribe(data=>{
       this.dataSource= new MatTableDataSource(data);
@@ -40,6 +43,8 @@ export class ProductoListarComponent implements OnInit {
     this.pS.eliminar(id).subscribe(() =>{
       this.pS.list().subscribe(data=>{
         this.pS.setList(data);
+        this.dataSource = new MatTableDataSource(data);
+        this.dataSource.paginator = this.paginator;
       })
     })
   }
